a74332e7abc8d116070b8ee6beca080f0fa59a25
Abdullah Al Noman <abdullahalnoman1512@gmail.com>
2021-06-04 17:05:16 +0600

    car item

---
+++ assets/Components/CarsList/Cars.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        name: 'Model s',
+        tagline: 'starting at $69,420',
+        image: require('../../images/ModelS.jpeg')
+    },
+
+    {
+        name: 'Model 3',
+        tagline: 'Order online for',
+        taglineCTA: 'Touchless Delivery',
+        image: require('../../images/Model3.jpeg')
+    },
+    {
+        name: 'Model X',
+        tagline: 'Order online for',
+        taglineCTA: 'Touchless Delivery',
+        image: require('../../images/ModelX.jpeg')
+    },
+
+    {
+        name: 'Model Y',
+        tagline: 'Order online for',
+        taglineCTA: 'Touchless Delivery',
+        image: require('../../images/ModelY.jpeg')
+    },
+
+]
