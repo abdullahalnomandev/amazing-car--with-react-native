14d58b4e3dd57732cb74157b0e8e601f2213932a
Abdullah Al Noman <abdullahalnoman1512@gmail.com>
2021-06-04 20:28:50 +0600

    done project

--- assets/Components/CarsList/Cars.js
+++ assets/Components/CarsList/Cars.js
@@ -2,27 +2,27 @@ export default [
     {
         name: 'Model s',
         tagline: 'starting at $69,420',
-        image: require('../../images/ModelS.jpeg')
+        image :{ uri: "https://images.unsplash.com/photo-1584957486338-8bfeeec844ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=282&q=80" }
     },
-
+    
     {
         name: 'Model 3',
         tagline: 'Order online for',
         taglineCTA: 'Touchless Delivery',
-        image: require('../../images/Model3.jpeg')
+        image: {uri: "https://images.unsplash.com/photo-1604483067049-dedef5afb913?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"}
     },
     {
         name: 'Model X',
         tagline: 'Order online for',
         taglineCTA: 'Touchless Delivery',
-        image: require('../../images/ModelX.jpeg')
+        image: {uri: "https://images.unsplash.com/photo-1594581823499-9d37751dc149?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"}
     },
 
     {
         name: 'Model Y',
         tagline: 'Order online for',
         taglineCTA: 'Touchless Delivery',
-        image: require('../../images/ModelY.jpeg')
+        image:{uri: "https://images.unsplash.com/photo-1615803522330-7b650c61153a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"}
     },
 
 ]
--- assets/Components/CarsList/styles.js
+++ assets/Components/CarsList/styles.js
@@ -4,9 +4,9 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
 
-
     container:{
         width:'100%',
+    
     }
 
 })
